aa1a254716cd5e3f7ba81d063ae87e492968c51a
Thomas Pearson <tompearson2002@gmail.com>
2021-02-08 21:44:52 +1300

    Noise v3

--- lib/ray_math/src/texture/noise.rs
+++ lib/ray_math/src/texture/noise.rs
@@ -1,6 +1,6 @@
 use rand::Rng;
 
-use crate::{Color, Point3};
+use crate::{Color, Point3, Vec3};
 
 use super::Texture;
 
@@ -20,12 +20,12 @@ impl Noise {
 
 impl Texture for Noise {
     fn value(&self, _uv: (f64, f64), point: &Point3) -> Color {
-        Color::one() * self.perlin.noise(&(self.scale * *point))
+        Color::one() * 0.5 * (1.0 + self.perlin.noise(&(self.scale * *point)))
     }
 }
 
 struct Perlin {
-    random: Vec<f64>,
+    random: Vec<Vec3>,
     perm_x: Vec<i32>,
     perm_y: Vec<i32>,
     perm_z: Vec<i32>,
@@ -36,7 +36,9 @@ const POINT_COUNT: i32 = 256;
 impl Perlin {
     fn new(rng: &mut dyn rand::RngCore) -> Self {
         Self {
-            random: (0..POINT_COUNT).map(|_| rng.gen_range(0.0..=1.0)).collect(),
+            random: (0..POINT_COUNT)
+                .map(|_| Vec3::random(rng, -1.0, 1.0).normalized())
+                .collect(),
             perm_x: Self::perlin_generate_perm(rng),
             perm_y: Self::perlin_generate_perm(rng),
             perm_z: Self::perlin_generate_perm(rng),
@@ -44,32 +46,27 @@ impl Perlin {
     }
 
     fn noise(&self, point: &Point3) -> f64 {
-        let mut u = point.x() - point.x().floor();
-        let mut v = point.y() - point.y().floor();
-        let mut w = point.z() - point.z().floor();
-
-        u = u * u * (3.0 - 2.0 * u);
-        v = v * v * (3.0 - 2.0 * v);
-        w = w * w * (3.0 - 2.0 * w);
+        let u = point.x() - point.x().floor();
+        let v = point.y() - point.y().floor();
+        let w = point.z() - point.z().floor();
 
         let i = point.x().floor() as i32;
         let j = point.y().floor() as i32;
         let k = point.z().floor() as i32;
 
-        let mut c = [[[0.0; 2]; 2]; 2];
-        for di in 0..2i32 {
-            for dj in 0..2i32 {
-                for dk in 0..2i32 {
-                    c[di as usize][dj as usize][dk as usize] = self.random[(self.perm_x
-                        [((i + di) & 255) as usize]
-                        ^ self.perm_y[((j + dj) & 255) as usize]
-                        ^ self.perm_z[((k + dk) & 255) as usize])
-                        as usize];
+        let mut c = [[[Vec3::zero(); 2]; 2]; 2];
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    let q = self.perm_x[((i + di as i32) & 255) as usize];
+                    let r = self.perm_y[((j + dj as i32) & 255) as usize];
+                    let s = self.perm_z[((k + dk as i32) & 255) as usize];
+                    c[di][dj][dk] = self.random[(q ^ r ^ s) as usize];
                 }
             }
         }
 
-        Self::trilinear_interp(&c, u, v, w)
+        Self::perlin_interp(&c, u, v, w)
     }
 
     fn perlin_generate_perm(rng: &mut dyn rand::RngCore) -> Vec<i32> {
@@ -85,7 +82,11 @@ impl Perlin {
         }
     }
 
-    fn trilinear_interp(c: &[[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+    fn perlin_interp(c: &[[[Vec3; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let uu = u * u * (3.0 - 2.0 * u);
+        let vv = v * v * (3.0 - 2.0 * v);
+        let ww = w * w * (3.0 - 2.0 * w);
+
         (0..2)
             .flat_map(move |i| {
                 (0..2).flat_map(move |j| {
@@ -93,10 +94,11 @@ impl Perlin {
                         let fi = i as f64;
                         let fj = j as f64;
                         let fk = k as f64;
-                        (fi * u + (1.0 - fi) * (1.0 - u))
-                            * (fj * v + (1.0 - fj) * (1.0 - v))
-                            * (fk * w + (1.0 - fk) * (1.0 - w))
-                            * c[i][j][k]
+                        let weight_v = Vec3::new(u - (i as f64), v - (j as f64), w - (k as f64));
+                        (fi * uu + (1.0 - fi) * (1.0 - uu))
+                            * (fj * vv + (1.0 - fj) * (1.0 - vv))
+                            * (fk * ww + (1.0 - fk) * (1.0 - ww))
+                            * c[i][j][k].dot(&weight_v)
                     })
                 })
             })
